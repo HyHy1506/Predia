97038835775076895e1465f79c36a887bce43a02
xuanduc <tran1106duc@gmail.com>
2024-08-03 11:35:37 +0700

    them chuc nang them file csv

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,redirect,render_template,request,url_for,jsonify
 import pickle
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 app=Flask(__name__)
@@ -21,23 +22,45 @@ def predict():
         # probability = model.predict_proba(scaled_features)
         # probability_of_diabetes = probability[0][1] * 100
         #-------create string
-        age = float(request.form['age'])
-        hypertension = float(request.form['hypertension'])
-        bmi = float(request.form['bmi'])
-        HbA1c_level = float(request.form['HbA1c_level'])
-        blood_glucose_level = float(request.form['blood_glucose_level'])
-        print(hypertension)
-        features = [age, hypertension, bmi, HbA1c_level, blood_glucose_level]
-        # chuyen list sang numpy.array
-        features_array = np.array(features).reshape(1, -1)
-        # chuan hoa
-        features_array_scaled=sc.transform(features_array)
-        # tinh phan tram 
-        probability = model.predict_proba(features_array_scaled)
-        probability_of_diabetes = probability[0][1] * 100
-        output = f"Bạn có khả năng: {probability_of_diabetes:.2f}% mắc bệnh tiểu đường"
-       
-        return render_template("predict.html",isPredicted=True, resultFinal=output,blood_glucose_level=blood_glucose_level,HbA1c_level=HbA1c_level,hypertension=hypertension,bmi=bmi,age=age)
+        useFile= 'useFile'in request.form
+        if useFile:
+            #xu ly dung file
+            file = request.files['file']
+            if file:
+                df=pd.read_csv(file,delimiter=';')
+
+                # xu ly csv
+                data_array = df.values#chuyen thanh numpy array
+                # chuan hoa
+                features_array_scaled=sc.transform(data_array)
+                # tinh phan tram 
+                probability = model.predict_proba(features_array_scaled)
+                
+                #chuyen du lieu vao html
+                data_array_new=np.column_stack((data_array,probability[:,1]))
+              
+                return render_template("predict.html",isPredicted=True,isListPatient=True,data_array=data_array_new)
+
+                #return render_template("predict.html",isPredicted=True,isListPatient=True,ageList=ageList,hypertensionList=hypertensionList,bmiList=bmiList,HbA1c_levelList=HbA1c_levelList,blood_glucose_levelList=blood_glucose_levelList)
+
+        else:
+             #xu ly khong dung file       
+            age = float(request.form['age'])
+            hypertension = float(request.form['hypertension'])
+            bmi = float(request.form['bmi'])
+            HbA1c_level = float(request.form['HbA1c_level'])
+            blood_glucose_level = float(request.form['blood_glucose_level'])
+            features = [age, hypertension, bmi, HbA1c_level, blood_glucose_level]
+            # chuyen list sang numpy.array
+            features_array = np.array(features).reshape(1, -1)
+            # chuan hoa
+            features_array_scaled=sc.transform(features_array)
+            # tinh phan tram 
+            probability = model.predict_proba(features_array_scaled)
+            probability_of_diabetes = probability[0][1] * 100
+            output = f"Bạn có khả năng: {probability_of_diabetes:.2f}% mắc bệnh tiểu đường"
+        
+            return render_template("predict.html",isPredicted=True,isListPatient=False, resultFinal=output,blood_glucose_level=blood_glucose_level,HbA1c_level=HbA1c_level,hypertension=hypertension,bmi=bmi,age=age)
     else:
         return render_template("predict.html",isPredicted=False)
 if __name__=="__main__":
--- static/js/predict.js
+++ static/js/predict.js
@@ -1,9 +1,33 @@
-let form =document.querySelector("#formEnterInfoID")
-console.log(form.classList)
+// let form =document.querySelector("#formEnterInfoID")
+// console.log(form.classList)
 
-form.addEventListener('submit',e=>{
-    if(!form.checkValidity()){
-    e.preventDefault()
+// form.addEventListener('submit',e=>{
+//     if(!form.checkValidity()){
+//     e.preventDefault()
+//     }
+// form.classList.add("was-validated")
+// })
+function toggleInputs() {
+    var useFileCheckbox = document.getElementById('checkUseFile');
+    var inputs = document.querySelectorAll('#so1, #so2, #so3, #so4, #so5');
+    var fileInput = document.getElementById('so6');
+    
+    if (useFileCheckbox.checked) {
+        inputs.forEach(function(input) {
+            input.disabled = true;
+        });
+        fileInput.disabled = false;
+    } else {
+        inputs.forEach(function(input) {
+            input.disabled = false;
+        });
+        fileInput.disabled = true;
     }
-form.classList.add("was-validated")
-})
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var useFileCheckbox = document.getElementById('checkUseFile');
+    useFileCheckbox.checked = false; // Đảm bảo rằng checkbox bị tắt khi trang được tải
+    toggleInputs(); // Gọi hàm này để đảm bảo trạng thái ban đầu là chính xác
+    useFileCheckbox.addEventListener('change', toggleInputs);
+});
